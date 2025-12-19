61b143ed29d02325cbc5ae0b728efc67ba404841
Paweł Fij <fi.pawel10@gmail.com>
2025-12-19 12:01:51 +0100

    Now the menu button works

--- src/renderer/ui.js
+++ src/renderer/ui.js
@@ -1,20 +1,18 @@
 // switching the right panell
-    let state = 0;
+let state = 1;
 
-function right_panel(){
+function right_panel() {
     const panel = document.querySelector(".rightPanel")
     const clicker = document.querySelector(".clicker")
-    if (state == 0){
+    if (state == 0) {
         panel.style.display = "none";
         clicker.style.width = "100vw";
-        document.getElementById("rightPanel").innerHTML = "&#8592";
         state = 1;
     }
-    else{
+    else {
         panel.style.display = "flex";
         clicker.style.width = "";
-        document.getElementById("rightPanel").innerHTML = "&#8594";
-        state =0;
+        state = 0;
     }
-    
+
 };
