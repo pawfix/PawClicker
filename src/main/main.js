a35b9c8cecf9a6b1cb6da03c8252c5c03bb9e3e9
Misiek1245 <embappe7@gmail.com>
2026-01-07 10:11:37 +0100

    changing resetdata to resetAllUserdata

--- src/main/main.js
+++ src/main/main.js
@@ -428,18 +428,34 @@ ipcMain.on('updateUserStats', (event, payload) => {
 
 });
 
-ipcMain.on('resetStatProgress', event => {
-    stats = { ...DEFAULT_STATS }; // make a copy to avoid reference issues
+ipcMain.on('resetAllUserData', event => {
+
+    // Reset user data to defaults since now it has some problems...
+ 
+    data = { ...DEFAULT_DATA};
+
+    shop = { ...DEFAULT_SHOP};
+
+    advancements = { ...DEFAULT_ADVANCEMENTS};
+
+    stats = { ...DEFAULT_STATS };
+
     saveAll();
+ 
+    //console.log(data, shop, advancements, stats);
 
     //console.log('reseted data');
-
+ 
     // Send updated stats to all windows
-    BrowserWindow.getAllWindows().forEach(win => {
-        win.webContents.send('getStatProgress', stats);
-    });
-});
 
+    BrowserWindow.getAllWindows().forEach(win => {
+
+        win.webContents.send('getStatProgress', stats);
+
+    });
+
+});
+ z` `
 
 /* =========================
    SHOP LOGIC
